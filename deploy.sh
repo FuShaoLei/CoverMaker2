7027a066872271f83872dbfdf8ea53c113553806
fushaolei <1563250958@qq.com>
2026-02-07 23:06:26 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 构建
+npm run build
+
+# 进入 dist 目录
+cd dist
+
+# 初始化 git
+git init
+
+# 添加远程仓库
+git remote add origin git@github.com:FuShaoLei/CoverMaker.git
+
+# 添加所有文件
+git add .
+
+# 提交
+git commit -m 'update'
+
+# 强制推送到 master 分支
+git push -f --set-upstream origin master
